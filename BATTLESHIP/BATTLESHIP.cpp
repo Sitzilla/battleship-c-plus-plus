949039dabe4eeffc3f29d0a649b27270d535966a
Evan Sitzes <esitzes42@hotmail.com>
2015-05-29 13:39:15 -0500

    created ship, board, and battleship classes

--- BATTLESHIP/BATTLESHIP.cpp
+++ BATTLESHIP/BATTLESHIP.cpp
@@ -2,10 +2,26 @@
 //
 
 #include "stdafx.h"
+#include "board.h"
+#include <iostream>
+using namespace std;
+
+Board::Board(){
+
+	// Initialize the board as a 10x10 multi-dimensional array
+	for (int row = 0; row < 10; row++) {
+		for (int col = 0; col < 10; col++) {
+			board[row][col] = false;
+		}
+	}
+
+}
 
 
 int _tmain(int argc, _TCHAR* argv[])
 {
+	Board gameboard;
+
 	return 0;
 }
 
